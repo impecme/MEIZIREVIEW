8306f1b881d40ad1e206eedebf133c6cf1695c5e
zhe dong <2741269405@qq.com>
2026-05-29 20:21:19 +0800

    test2

--- auto_git.sh
+++ auto_git.sh
@@ -1,16 +1,13 @@
-#!/usr/bin/env bash
-set -e
-
-msg="$*"
-
-if [ -z "$msg" ]; then
-  read -r -p "请输入提交名称: " msg
-fi
 git add -- \
   auto_git.sh \
   微机原理 \
   半导体物理 \
   Markdown基础用法.md \
 
-git commit -m "$msg"
+if git diff --cached --quiet; then
+	echo "No staged changes to commit."
+	exit 1
+fi
+
+git commit -m "$1"
 git push
